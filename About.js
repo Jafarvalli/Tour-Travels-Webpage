48a9331278982dcb2c2dcb7671a483b5be25bfed
NADENDLA JAFAR VALLI <100751484+Jafarvalli@users.noreply.github.com>
2022-05-08 20:25:11 -0700

    Create About.js

---
+++ About.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './About.css';
+
+function About(){
+    return(
+        <div className="about">
+            <div className="img">
+                <img src="https://images.newindianexpress.com/uploads/user/imagelibrary/2019/3/6/w900X450/Fleet.jpg?w=400&dpr=2.6" alt="img1"/>
+            </div>
+           <div className="content">
+               <h2>About Us</h2>
+               <div className="content1">
+               <p><span>Jafar Tours & Travels</span> is a pioneer in the online bus reservation in India which utilizes the advantages of the internet to provide our clients with the best in bus transportation services from the comforts of their homes and offices.Ovr Tours & Travels provides real-time Internet quotations and real-time bus booking services to direct customers, agents and tour operator partners.</p>
+               <p>The idea for<span>Jafar Tours & Travels</span>  has stemmed from the need for simple travel solutions. Buses are the most commonly used form of travel in India, be it within a city or intra-city. In fact, buses are sometimes the only form of transportation available to reach the remote parts of India. However, reserving a seat on a bus is one of the most tedious processes. Serpentine queues and heated arguments with staff at reservation counters has been the scene at most Indian bus stations.Ovr Tours & Travels aims to transform this scene to the one of reserving bus seats from the luxury of ones homes. As the pioneering online bus reservation company,<span>Jafar Tours & Travels</span> aims to provide our customers with a wide range of services unsurpassed by any competitor in this industry. We are geared to become the top seller among online travel companies for all the major bus transportation providers in India.</p>
+               <p>As a company,<span>Jafar Tours & Travels</span> believes in engineering win-win relationships with our partners in order to guarantee ongoing benefits to our customers. The very nature of our reservation systems demands that they are not only the best today, but can remain the best for years to come without the need for disruptive ongoing development. With our systems, we are sure we would be able to provide uninterrupted service to our customers and partners Heres to making bus travel more fun even before you get into one!!</p>
+               </div>
+           </div>
+           
+        </div>
+
+    )
+}
+
+export default About;
