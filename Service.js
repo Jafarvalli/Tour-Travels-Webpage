c517bba108ddaad899dda37f2cf9c8a57b9dcb47
NADENDLA JAFAR VALLI <100751484+Jafarvalli@users.noreply.github.com>
2022-05-08 20:26:35 -0700

    Create Service.js

---
+++ Service.js
@@ -0,0 +1,47 @@
+import React from "react";
+import './Service.css';
+
+function Services(){
+    return(
+        <div className="head">
+            <h3>Our Services on Various Places are</h3>
+            <section className="grid1">
+                <div>
+                <p>Pulivendula</p>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS88gn_vSasIb6ubg9zBsOzMiotnLwyNVtW2zX4tnjUuwXMs_UjZx5oSbZH3itqK0SyIA&usqp=CAU" alt="img1"/>
+                </div>
+                <div>
+                <p>Kadapa</p>
+                <img src="https://assets.thehansindia.com/h-upload/2019/10/15/226249-four-lane-road.jpg" alt="img1"/>
+                </div>
+                <div>
+                <p>Anantapur</p>
+                <img src="https://assets.thehansindia.com/h-upload/feeds/2019/04/27/168818-it-hub.jpg" alt="img1"/>
+                </div>
+                <div>
+                <p>Kurnool</p>
+                <img src="https://images.newindianexpress.com/uploads/user/imagelibrary/2021/2/25/w1200X800/Kurnooln.jpg" alt="img1"/>
+                </div>
+                <div>
+                <p>Bangalore</p>
+                <img src="https://akm-img-a-in.tosshub.com/businesstoday/images/story/201802/coinn_660_022618014547.jpg"alt="img1"/>
+                </div>
+                <div>
+                <p>Hyderabad</p>
+                <img src="https://telanganatoday.com/wp-content/uploads/2020/11/transform.jpg" alt="img1"/>
+                </div>
+                <div>
+                <p>Chennai</p>
+                <img src="https://im.rediff.com/news/2014/aug/25chennai1.jpg" alt="img1"/>
+                </div>
+                <div>
+                <p>Kerala</p>
+                <img src="https://img.theculturetrip.com/wp-content/uploads/2018/03/wayanad_churam_thamarasseri-sreeraj-ps-aka-ezhuttukari-wikicommons.jpg" alt="img1"/>
+                </div>
+            </section>
+            
+        </div>
+    )
+}
+
+export default Services;
